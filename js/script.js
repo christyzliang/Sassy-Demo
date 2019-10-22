80a85c54dd8b585251cefb51034234736c2677ec
christyzliang <46726448+christyzliang@users.noreply.github.com>
2019-10-22 12:29:40 -1000

    Added script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const mysitenav = document.querySelector('header .site-nav');
+const mymenubutton = document.querySelector('menu-button');
+
+console.log(clicked!)
+
+mymenubutton.onclick = fucntion() {
+    mysitenav.toggle('Active');
+}
