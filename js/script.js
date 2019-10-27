ab34db253c427708231a66ff50774b369188d40c
christyzliang <46726448+christyzliang@users.noreply.github.com>
2019-10-26 21:48:08 -1000

    Updated menu bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-const mysitenav = document.querySelector('header .site-nav');
+const mysitenav = document.querySelector('.site-header .site-nav');
 const mymenubutton = document.querySelector('.menu-button');
 
-console.log('clicked!')
-
-mymenubutton.onclick = function() {
+mymenubutton.onclick = function () {
     mysitenav.classList.toggle('active');
 }
+// Remove active class when user clicks on overlay nav links//
+const mysitenavlinks = document.querySelectorAll('.site-header .site-nav a');
+for (var i = 0, len = mysitenavlinks.length; i < len; i++) {
+    mysitenavlinks[i].onclick = function () {
+        mysitenav.classList.remove('active');
+    };
+};
